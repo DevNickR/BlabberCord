11b336fe85fe4023bf4e59a7a52f98f4d112e0f7
Nick <130463132+DevNickR@users.noreply.github.com>
2023-05-07 18:09:16 +1000

    fix update dockerhub desc script

--- update_dockerhub_description.sh
+++ update_dockerhub_description.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-set -e
-
+# Required parameters
 USERNAME=$1
 PASSWORD=$2
 REPOSITORY=$3
 README_FILEPATH=$4
 
-# Obtain a token from Docker Hub
+# Authenticate with Docker Hub
 TOKEN=$(curl -s -X POST -H "Content-Type: application/json" -d '{"username": "'${USERNAME}'", "password": "'${PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
 
-# Get the current README content from the file
-README_CONTENT=$(<$README_FILEPATH)
+# Prepare the payload
+README_PAYLOAD=$(<${README_FILEPATH} jq -sR .)
 
-# Update the Docker Hub repository description
-curl -s -X PATCH -H "Content-Type: application/json" -H "Authorization: JWT ${TOKEN}" -d '{"full_description": "'"${README_CONTENT//$'\n'/'\n'}"'"}' "https://hub.docker.com/v2/repositories/${REPOSITORY}/"
+# Update the Docker Hub description
+curl -s -X PATCH -H "Content-Type: application/json" -H "Authorization: JWT ${TOKEN}" -d "{\"full_description\": ${README_PAYLOAD}}" https://hub.docker.com/v2/repositories/${REPOSITORY}/
 
-echo "Docker Hub description updated successfully!"
